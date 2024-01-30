4cd70e7f1f21b4a8cf32f6a5f37f0a608d08d74e
Daniel M. Drucker <dmd@3e.org>
2024-01-30 16:49:47 -0500

    i like iso dates

--- server/index.ts
+++ server/index.ts
@@ -67,7 +67,7 @@ class Room {
     if (!this.messages[socket.id]?.slice(-2)[0]?.includes("has joined")) {
       this.messages[socket.id].push(
         `> socket id ${socket.id} has joined at ${
-          new Date().toLocaleString("en-US", { timeZoneName: "short" })
+          new Date().toISOString().replace('T',' ').split('.')[0]+"Z"
         }`,
       );
       this.messages[socket.id].push("");
@@ -82,7 +82,7 @@ class Room {
     this.sockets = this.sockets.filter((socket) => socket.id !== id);
     this.messages[id].push(
       `> socket id ${id} has left at ${
-        new Date().toLocaleString("en-US", { timeZoneName: "short" })
+        new Date().toISOString().replace('T',' ').split('.')[0]+"Z"
       }`,
     );
     this.messages[id].push("");
